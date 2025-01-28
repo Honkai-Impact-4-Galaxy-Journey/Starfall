373b20ad49709b1e140a0e2fb2f098adeb3d2cf8
Griseo-bh3rd <atmlz32@163.com>
2025-01-29 01:25:02 +0800

    feat: 玩家信息模型，连接池（WIP）

---
+++ Starinfall/Database.cs
@@ -0,0 +1,50 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starinfall
+{
+    public class PlayerInfo
+    {
+        public string Steamid { get; set; } // 不会变动，需要初始化
+        public string LastNickName { get; set; }
+        public string BadgeText { get; set; } // 不会变动
+        public string BadgeColor { get; set; } // 不会变动
+        public DateTime LastJoinTime { get; set; }
+        public bool ReserveSlotEnabled { get; set; } // 不会变动
+        public string LastIP { get; set; }
+        public int Id { get; set; } // 不会变动
+        public string JoinMessage { get; set; } // 不会变动
+        public int Exp { get; set; }
+        public int Level { get; set; }
+        public string AdminRank { get; set; } // 不会变动
+        public bool Cover { get; set; } // 不会变动
+        public bool Banned { get; set; }
+        public bool Modified = false;
+    }
+    public class Database
+    {
+
+    }
+    public class ConnectionPool
+    {
+        private static Queue<MySqlConnection> mySqls = new Queue<MySqlConnection>();
+        public const int InitCounts = 3;
+        public static void InitConnectionPool()
+        {
+            for(int i = 0; i < InitCounts; ++i)
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection(PluginMain.Instance.Config.ConnectionString);
+                mySqlConnection.Open();
+                mySqls.Enqueue(mySqlConnection);
+            }
+        }
+        public static MySqlConnection GetConnection()
+        {
+            
+        }
+    }
+}
--- Starinfall/PluginMain.cs
+++ Starinfall/PluginMain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,16 +8,19 @@ using PluginAPI.Core.Attributes;
 
 namespace Starinfall
 {
-    public class PluginConfig
+    public class MainConfig
     {
-        
+        [Description("MySQL连接字符串")]
+        public string ConnectionString { get; set; } = "server=127.0.0.1;database=scp;user=root;password=123456;charset=utf-8";
     }
     public class PluginMain
     {
+        [PluginConfig] public MainConfig Config;
+        public static PluginMain Instance { get; private set; }
         [PluginEntryPoint("Starinfall","1.0.0","-","Honkai Impact 4: Galaxy Journey Team")]
         public void OnEnabled()
         {
-
+            Instance = this;
         }
     }
 }
